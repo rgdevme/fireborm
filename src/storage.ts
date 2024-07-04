bdfd9fbff48e22c08d6a838b387dc312bfbb1a71
Rafael González <reggon81@gmail.com>
2024-07-04 12:58:18 -0400

    add storage class

---
+++ src/storage.ts
@@ -0,0 +1,45 @@
+import {
+  FirebaseStorage,
+  StorageReference,
+  deleteObject,
+  getDownloadURL,
+  listAll,
+  ref,
+  uploadBytes,
+} from 'firebase/storage'
+
+export class FirebormStorage {
+  readonly path: string
+  #ref?: StorageReference
+
+  constructor({ path, folder }: { path: string; folder: string }) {
+    this.path = `${path}/${folder}`
+  }
+
+  public init = (storage: FirebaseStorage) => {
+    if (this.#ref) throw new Error('Bucket has been initialized already')
+    this.#ref = ref(storage, this.path)
+  }
+
+  get ref() {
+    if (!this.#ref) throw new Error("Bucket hasn't been initialized")
+    return this.#ref
+  }
+
+  public getFileRef = (name: string) => ref(this.ref, name)
+
+  public upload = async (name: string, file: File) => {
+    const fileref = this.getFileRef(name)
+    const { ref } = await uploadBytes(fileref, file)
+    return getDownloadURL(ref)
+  }
+  public download = async (name: string) => {
+    const fileref = this.getFileRef(name)
+    return getDownloadURL(fileref)
+  }
+  public remove = async (name: string) => {
+    const fileref = this.getFileRef(name)
+    return deleteObject(fileref)
+  }
+  public list = async () => listAll(this.ref)
+}
