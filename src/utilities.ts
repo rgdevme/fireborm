e47af85ffec882df7ffb371c5cdacf44245289a0
Rafael González <reggon81@gmail.com>
2024-07-15 13:08:58 -0400

    fix: fix type

--- src/utilities.ts
+++ src/utilities.ts
@@ -5,7 +5,7 @@ export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>
 export type Prioritize<T, K extends keyof T> = Required<Pick<T, K>> &
   Partial<Omit<T, K>>
 export type KeysMatching<T extends object, V> = {
-  [K in keyof T]-?: T[K] extends V ? K : never
+  [K in keyof T]-?: Required<T>[K] extends V ? K : never
 }[keyof T]
 export type WithID<T> = T & { id: string }
 export type WithRef<T, Ref extends DocumentReference> = T & {
