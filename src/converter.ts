9ea1ccbb7a4bed804a68e4a41c869c03d57fe0b5
Rafael González <reggon81@gmail.com>
2024-11-26 14:13:43 +0100

    fix: fix, add, and export types

--- src/converter.ts
+++ src/converter.ts
@@ -1,27 +1,14 @@
-import {
-  DocumentData,
-  DocumentReference,
-  QueryDocumentSnapshot,
-} from 'firebase/firestore'
-
-export type ConvertedModel<
-  D extends DocumentData,
-  M extends D | DocumentData
-> = M & {
-  id: string
-  _ref: DocumentReference<M, D>
-}
+import { DocumentData, QueryDocumentSnapshot } from 'firebase/firestore'
 
 export type FSConverter<D extends DocumentData, M extends D | DocumentData> = {
-  toFirestore: (model: M) => D
-  fromFirestore: (doc: QueryDocumentSnapshot<D, D>) => M
+	toFirestore: (model: M) => D
+	fromFirestore: (doc: QueryDocumentSnapshot<D, D>) => M
 }
 
 export const defaultConverter = <
-  D extends DocumentData,
-  M extends D | DocumentData
+	D extends DocumentData,
+	M extends D | DocumentData
 >(): FSConverter<D, M> => ({
-  toFirestore: (model) => model as D,
-  fromFirestore: doc => doc.data() as M
-}
-)
+	toFirestore: model => model as D,
+	fromFirestore: doc => doc.data() as M
+})
--- src/utilities.ts
+++ src/utilities.ts
@@ -1,50 +1,73 @@
-import { DocumentReference } from 'firebase/firestore'
+import { DocumentData, DocumentReference } from 'firebase/firestore'
 
 /** Modifies the properties of the first type,
- * overwriting them with the properties of the second type second object 
+ * overwriting them with the properties of the second type second object
  **/
-export type Modify<
-  Base,
-  Mods extends { [k in keyof Base]?: any }
-> = Omit<Base, keyof Mods> & Mods
+export type Modify<Base, Mods extends { [k in keyof Base]?: any }> = Omit<
+	Base,
+	keyof Mods
+> &
+	Mods
 
 /** Make only some properties optional */
-export type Optional<Base, K extends keyof Base> =
-  Pick<Partial<Base>, K> & Omit<Base, K>
+export type Optional<Base, K extends keyof Base> = Pick<Partial<Base>, K> &
+	Omit<Base, K>
 
 /** Make only some properties not optional */
 export type Prioritize<T, K extends keyof T> = Required<Pick<T, K>> &
-  Partial<Omit<T, K>>
+	Partial<Omit<T, K>>
 
 export type WithID<T> = T & { id: string }
 
 export type WithRef<T, Ref extends DocumentReference> = T & {
-  _ref: Ref
+	_ref: Ref
 }
 
 /** Return keys that match type */
 export type PickByType<T extends Record<string, any>, V> = {
-  [K in keyof T as T[K] extends infer U
-  ? (U extends V ? K : never)
-  : never]
-  : T[K]
+	[K in keyof T as T[K] extends infer U
+		? U extends V
+			? K
+			: never
+		: never]: T[K]
 }
 
 /** Return keys that match type strictly */
 export type PickByTypeStrict<T extends Record<string, any>, V> = {
-  [K in keyof T as T[K] extends V ? K : never]: T[K] }
+	[K in keyof T as T[K] extends V ? K : never]: T[K]
+}
 
 /** Returns a new type with only the optional keys of the given type */
-export type PickOptionals<T extends Record<string, any>> = PickByType<T, undefined>
+export type PickOptionals<T extends Record<string, any>> = PickByType<
+	T,
+	undefined
+>
 
 /** Returns the base type with all optional keys extending to null too */
 export type NullOptionals<T extends Record<string, any>> = {
-  [K in keyof T]: T[K] extends infer U
-  ? (U extends undefined ? T[K] | null : T[K])
-  : T[K]
+	[K in keyof T]: T[K] extends infer U
+		? U extends undefined
+			? T[K] | null
+			: T[K]
+		: T[K]
 }
 
-export type NullByKey<
-  T extends Record<string, any>,
-  X extends keyof T
-> = { [K in keyof T]: K extends X ? T[K] | null : T[K] }
+export type NullByKey<T extends Record<string, any>, X extends keyof T> = {
+	[K in keyof T]: K extends X ? T[K] | null : T[K]
+}
+
+export type ConvertedModel<
+	D extends DocumentData,
+	M extends D | DocumentData = {}
+> = Modify<
+	D,
+	M & {
+		id: string
+		_ref: DocumentReference<ThisType<ConvertedModel<D, M>>, D>
+	}
+>
+
+export type DefaultModel<M extends Object> = Omit<
+	M,
+	keyof PickOptionals<M> | 'id' | '_ref'
+>
