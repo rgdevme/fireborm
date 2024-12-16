94176c3cfee586578a60cbd68fe7c5bd698db7fe
Rafael González <reggon81@gmail.com>
2024-12-16 19:11:52 +0100

    fix: change external deps. autoformat.

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,34 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig(({ command }) => ({
-  root: './',
-  build: {
-    watch: command === 'serve' ? {
-      buildDelay: 2000,
-      clearScreen: true,
-      include: 'src/**',
-    } : undefined,
-    lib: {
-      entry: './src/index.ts',
-      name: 'index',
-      fileName: 'index',
-      formats: ['cjs', 'es'],
-    },
-    outDir: 'dist',
-    emptyOutDir: false,
-    minify: false,
-    rollupOptions: {
-      external: [/firebase/, 'currency.js'],
-    },
-  },
-  plugins: [
-    dts({
-      include: ['./src'],
-      rollupTypes: true,
-      insertTypesEntry: true,
-    }),
-  ],
+	root: './',
+	build: {
+		watch:
+			command === 'serve'
+				? {
+						buildDelay: 2000,
+						clearScreen: true,
+						include: 'src/**'
+				  }
+				: undefined,
+		lib: {
+			entry: './src/index.ts',
+			name: 'index',
+			fileName: 'index',
+			formats: ['cjs', 'es']
+		},
+		outDir: 'dist',
+		emptyOutDir: false,
+		minify: false,
+		rollupOptions: {
+			external: [/firebase/, /node_modules/]
+		}
+	},
+	plugins: [
+		dts({
+			include: ['./src'],
+			rollupTypes: true,
+			insertTypesEntry: true
+		})
+	]
 }))
